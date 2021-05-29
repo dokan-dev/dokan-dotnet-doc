55ae732ab2817d3b83d6eb575d404c2326f63a15
appveyor <appveyor@appveyor.org>
2021-05-29 18:39:33 +0000

    Latest documentation on successful appveyor build 1.3.1-22 auto-pushed to gh-pages

--- html/search/properties_2.js
+++ html/search/properties_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['debugenabled',['DebugEnabled',['../interface_dokan_net_1_1_logging_1_1_i_logger.html#a30e3071d0bc32f5725d51b9e3cd60395',1,'DokanNet::Logging::ILogger']]],
   ['deleteonclose',['DeleteOnClose',['../class_dokan_net_1_1_dokan_file_info.html#abe8f97a17b7e66b4a7013a08c7730f5b',1,'DokanNet.DokanFileInfo.DeleteOnClose()'],['../interface_dokan_net_1_1_i_dokan_file_info.html#af5c7919408fa8828b1d5166c25d137f7',1,'DokanNet.IDokanFileInfo.DeleteOnClose()'],['../class_dokan_net_1_1_mock_dokan_file_info.html#a3c7d9166afe2652505f65fb9556e35e3',1,'DokanNet.MockDokanFileInfo.DeleteOnClose()']]]
 ];
--- html/search/variables_2.js
+++ html/search/variables_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['debugenabled',['DebugEnabled',['../class_dokan_net_1_1_logging_1_1_console_logger.html#ac432c2717bc6f3230308fe1cb50bd275',1,'DokanNet.Logging.ConsoleLogger.DebugEnabled()'],['../class_dokan_net_1_1_logging_1_1_debug_view_logger.html#a35141e15024029b655e9219a39340585',1,'DokanNet.Logging.DebugViewLogger.DebugEnabled()'],['../class_dokan_net_1_1_logging_1_1_logger.html#a71d4258fdf53de2b6b0075eefd76732f',1,'DokanNet.Logging.Logger.DebugEnabled()'],['../class_dokan_net_1_1_logging_1_1_null_logger.html#a1f631c2e5e2d23b3e884123cd0cf2322',1,'DokanNet.Logging.NullLogger.DebugEnabled()'],['../class_dokan_net_1_1_logging_1_1_trace_logger.html#a1bf98f57fa143df0af078f4196097e02',1,'DokanNet.Logging.TraceLogger.DebugEnabled()']]],
   ['defaultformatprovider',['DefaultFormatProvider',['../class_dokan_net_1_1_format_providers.html#a5e2da0dc7099791e7afb64e78e868be5',1,'DokanNet::FormatProviders']]],
   ['directorynotempty',['DirectoryNotEmpty',['../class_dokan_net_1_1_dokan_result.html#aeb75c7b138f7169ea981e788af53e2e5',1,'DokanNet::DokanResult']]],
   ['diskfull',['DiskFull',['../class_dokan_net_1_1_dokan_result.html#a4d35352518c67540f49faedda8830060',1,'DokanNet::DokanResult']]],
