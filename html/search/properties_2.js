fec5ae7969889ba5f9bf39a6a09acbef512f2331
appveyor <appveyor@appveyor.org>
2025-04-26 19:01:33 +0000

    Latest documentation on successful appveyor build 2.2.1-122 auto-pushed to gh-pages

--- html/search/properties_2.js
+++ html/search/properties_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['debugenabled',['DebugEnabled',['../interface_dokan_net_1_1_logging_1_1_i_logger.html#a30e3071d0bc32f5725d51b9e3cd60395',1,'DokanNet::Logging::ILogger']]],
-  ['deleteonclose',['DeleteOnClose',['../class_dokan_net_1_1_dokan_file_info.html#abe8f97a17b7e66b4a7013a08c7730f5b',1,'DokanNet.DokanFileInfo.DeleteOnClose()'],['../interface_dokan_net_1_1_i_dokan_file_info.html#af5c7919408fa8828b1d5166c25d137f7',1,'DokanNet.IDokanFileInfo.DeleteOnClose()'],['../class_dokan_net_1_1_mock_dokan_file_info.html#a3c7d9166afe2652505f65fb9556e35e3',1,'DokanNet.MockDokanFileInfo.DeleteOnClose()']]]
+  ['deletepending',['DeletePending',['../class_dokan_net_1_1_dokan_file_info.html#afd161145fcbe2dba18e9c856d5f98611',1,'DokanNet.DokanFileInfo.DeletePending()'],['../interface_dokan_net_1_1_i_dokan_file_info.html#a6a8de29c93ed09f0b99236d4afcfc75b',1,'DokanNet.IDokanFileInfo.DeletePending()'],['../class_dokan_net_1_1_mock_dokan_file_info.html#a4207a58e9fb3408aa6788bf6c8d3f623',1,'DokanNet.MockDokanFileInfo.DeletePending()']]]
 ];
